39b62c75fb0b8dd6621ddd64f8d17c1778d85993
Talha <talharamzan704@gmail.com>
2025-08-08 00:15:37 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -187,5 +187,19 @@
             }
         });
     });
+	
+/**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+  
 
 })(jQuery);
