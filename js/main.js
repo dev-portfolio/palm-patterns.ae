ba721874e9636b5ecaa137c4089c71a871d5c89e
Talha <talharamzan704@gmail.com>
2025-08-20 11:53:37 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,8 +17,32 @@
     $(window).on('load', function () {
         $(".loader").fadeOut();
         $("#preloder").delay(200).fadeOut("slow");
+		AOS.init({
+		  duration: 600,
+		  easing: 'ease-in-out',
+		  once: true,
+		  mirror: false
+		});
     });
+  function toggleScrolled() {
+    const selectBody = document.querySelector('body');
+    const selectHeader = document.querySelector('#header');
+    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+  }
 
+  document.addEventListener('scroll', toggleScrolled);
+  window.addEventListener('load', toggleScrolled);
+
+  // function aosInit() {
+    // AOS.init({
+      // duration: 600,
+      // easing: 'ease-in-out',
+      // once: true,
+      // mirror: false
+    // });
+  // }
+  // window.addEventListener('load', aosInit);
     /*------------------
         Background Set
     --------------------*/
@@ -39,6 +63,7 @@
     });
 
 
+
     /*------------------
 		Navigation
 	--------------------*/
@@ -47,37 +72,37 @@
         allowParentLinks: true
     });
 
-    /*-----------------------
-        Hero Slider
-    ------------------------*/
-    $(".hero__slider").owlCarousel({
-        loop: true,
-        margin: 0,
-        items: 1,
-        dots: true,
-        nav: true,
-        navText: ["<i class='fa fa-angle-left'><i/>", "<i class='fa fa-angle-right'><i/>"],
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        smartSpeed: 1500,
-        autoHeight: false,
-        autoplay: true,
-        mouseDrag: false,
-        onInitialized: function (e) {
-            var a = this.items().length;
-            $("#snh-1").html("<span>01</span><span>" + "0" + a + "</span>");
-            var presentage = Math.round((100 / a));
-            $('.slider__progress span').css("width", presentage + "%");
-        }
-    }).on("changed.owl.carousel", function (e) {
-        var b = --e.item.index,
-            a = e.item.count;
-        $("#snh-1").html("<span> " + "0" + (1 > b ? b + a : b > a ? b - a : b) + "</span><span>" + "0" + a + "</span>");
+    // /*-----------------------
+        // Hero Slider
+    // ------------------------*/
+    // $(".hero__slider").owlCarousel({
+        // loop: true,
+        // margin: 0,
+        // items: 1,
+        // dots: true,
+        // nav: true,
+        // navText: ["<i class='fa fa-angle-left'><i/>", "<i class='fa fa-angle-right'><i/>"],
+        // animateOut: 'fadeOut',
+        // animateIn: 'fadeIn',
+        // smartSpeed: 1500,
+        // autoHeight: false,
+        // autoplay: true,
+        // mouseDrag: false,
+        // onInitialized: function (e) {
+            // var a = this.items().length;
+            // $("#snh-1").html("<span>01</span><span>" + "0" + a + "</span>");
+            // var presentage = Math.round((100 / a));
+            // $('.slider__progress span').css("width", presentage + "%");
+        // }
+    // }).on("changed.owl.carousel", function (e) {
+        // var b = --e.item.index,
+            // a = e.item.count;
+        // $("#snh-1").html("<span> " + "0" + (1 > b ? b + a : b > a ? b - a : b) + "</span><span>" + "0" + a + "</span>");
 
-        var current = e.page.index + 1;
-        var presentage = Math.round((100 / e.page.count) * current);
-        $('.slider__progress span').css("width", presentage + "%");
-    });
+        // var current = e.page.index + 1;
+        // var presentage = Math.round((100 / e.page.count) * current);
+        // $('.slider__progress span').css("width", presentage + "%");
+    // });
 
     /*--------------------------
         Project Slider
@@ -104,74 +129,75 @@
             },
             1200: {
                 items: 4
-            }
+            },
+
         }
     });
 
-    /*-----------------------------
-        Testimonial Slider
-    -------------------------------*/
-    $('.testimonial__carousel').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        fade: true,
-        // autoplay:true,
-        asNavFor: '.testimonial__client',
-        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"><i></i></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"><i></i></i></button>',
-      });
-      $('.testimonial__client').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        asNavFor: '.testimonial__carousel',
-        arrows: false,
-        variableWidth: true,
-        centerMode: true,
-        focusOnSelect: true,
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 3,
-              }
-            },
-            {
-              breakpoint: 575,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                variableWidth: false
-              }
-            }
-          ]
+    // /*-----------------------------
+        // Testimonial Slider
+    // -------------------------------*/
+    // $('.testimonial__carousel').slick({
+        // slidesToShow: 1,
+        // slidesToScroll: 1,
+        // arrows: true,
+        // fade: true,
+        // // autoplay:true,
+        // asNavFor: '.testimonial__client',
+        // prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"><i></i></i></button>',
+        // nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"><i></i></i></button>',
+      // });
+      // $('.testimonial__client').slick({
+        // slidesToShow: 4,
+        // slidesToScroll: 1,
+        // asNavFor: '.testimonial__carousel',
+        // arrows: false,
+        // variableWidth: true,
+        // centerMode: true,
+        // focusOnSelect: true,
+        // responsive: [
+            // {
+              // breakpoint: 768,
+              // settings: {
+                // slidesToShow: 3,
+              // }
+            // },
+            // {
+              // breakpoint: 575,
+              // settings: {
+                // slidesToShow: 1,
+                // slidesToScroll: 1,
+                // variableWidth: false
+              // }
+            // }
+          // ]
         
-      });
+      // });
 
     /*---------------------------------
         Logo Carousel
     ----------------------------------*/
-    $(".logo__carousel").owlCarousel({
-        loop: true,
-        margin: 80,
-        items: 5,
-        dots: false,
-        nav: false,
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true,
-        responsive: {
-            320: {
-                items: 2
-            },
-            576: {
-                items: 3
-            },
-            992: {
-                items: 5
-            },
-        }
-    });
+    // $(".logo__carousel").owlCarousel({
+        // loop: true,
+        // margin: 80,
+        // items: 5,
+        // dots: false,
+        // nav: false,
+        // smartSpeed: 1200,
+        // autoHeight: false,
+        // autoplay: true,
+        // responsive: {
+            // 320: {
+                // items: 2
+            // },
+            // 576: {
+                // items: 3
+            // },
+            // 992: {
+                // items: 5
+            // },
+        // }
+    // });
 
     /*------------------
         Counter
@@ -191,15 +217,7 @@
 /**
    * Animation on scroll function and init
    */
-  function aosInit() {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    });
-  }
-  window.addEventListener('load', aosInit);
+
   
 
 })(jQuery);
